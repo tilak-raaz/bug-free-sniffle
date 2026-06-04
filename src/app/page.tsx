afd11824b8cae41d819a59e29eeb7c41a2dcf717
Suyash Aditya <suyashaditya919@gmail.com>
2026-06-04 11:23:51 +0530

    suyash initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
       <div className="font-canopee bg-secondary text-background text-shadow hack-shadow mx-auto my-auto w-2xl py-4 text-center text-5xl">
         happy coding!
       </div>
+      <div className="font-canopee bg-secondary text-background text-shadow hack-shadow mx-auto my-10 w-2xl py-4 text-center text-5xl">
+        hello everyone, this is Suyash!
+      </div>
     </div>
   );
 }
