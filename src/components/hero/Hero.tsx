87f4c7865fdccaa5fcb0fcf311a2fe2bd6116af0
Sairaj Tripathy <sairajtripathy4@gmail.com>
2026-07-09 21:23:14 +0530

    fix: change next lint to eslint src and resolve unused import warning

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,6 @@
 'use client';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { Menu, X } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
